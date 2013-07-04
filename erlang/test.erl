f4a320a39de5ce8bf521f4d3baa6293c25d83226
jabir ahmed <jabirahmed@yahoo.com>
2013-07-05 00:12:35 +0530

    added new function

--- erlang/test.erl
+++ erlang/test.erl
@@ -1,5 +1,5 @@
 -module(test).
--export([add/2,hello/0,greet_and_add_two/1]).
+-export([add/2,hello/0,greet_and_add_two/1,greet_gender/2]).
 
 add(A,B)->
 	A+B.
@@ -12,3 +12,11 @@ hello()->
 greet_and_add_two(X)->
 	hello(),
 	add(X,2).
+
+greet_gender(male,Name)->
+	io:format("Hello Mr. ~s !",[Name]);
+greet_gender(female,Name)->
+	io:format("Hello Mrs. ~s !",[Name]);
+greet_gender(_,Name)->
+	io:format("Hello ~s ", [Name]).
+
