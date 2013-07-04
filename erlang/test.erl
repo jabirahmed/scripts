205b08b8d43359b706ececfcb2dc5ab96734d6a7
jabir ahmed <jabirahmed@yahoo.com>
2013-07-04 23:59:30 +0530

    test erlang

---
+++ erlang/test.erl
@@ -0,0 +1,14 @@
+-module(test).
+-export([add/2,hello/0,greet_and_add_two/1]).
+
+add(A,B)->
+	A+B.
+
+
+hello()->
+   io:format("hello world!~n").
+
+
+greet_and_add_two(X)->
+	hello(),
+	add(X,2).
